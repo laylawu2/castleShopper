b6db10e2298092f568d4dcaae1f26c81c4481606
anniejiwon <anniejiwon@gmail.com>
2016-11-01 17:45:07 -0400

    added some structure to homepage, added and linked css file

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -1,4 +1,29 @@
 import React from 'react';
 export default ({}) => (
-	<div>Home Page</div>
+	<div>
+		<div>Home Page</div>
+		<div className="row">
+			<div className="col-xs-6 col-md-3">
+				<a href="#" className="thumbnail">
+				<img src="https://cdn.shopify.com/s/files/1/0249/1218/products/Vitamin-C-Serum-BoB_ea089a86-dbaf-454b-9d87-d4ce5cca4bb1_large.jpg?v=1460043054"></img>
+				</a>
+			</div>
+			<div className="col-xs-6 col-md-3">
+				<a href="#" className="thumbnail">
+				<img src="https://cdn.shopify.com/s/files/1/0249/1218/products/Vitamin-C-Serum-BoB_ea089a86-dbaf-454b-9d87-d4ce5cca4bb1_large.jpg?v=1460043054"></img>
+				</a>
+			</div>
+			<div className="col-xs-6 col-md-3">
+				<a href="#" className="thumbnail">
+				<img src="https://cdn.shopify.com/s/files/1/0249/1218/products/Vitamin-C-Serum-BoB_ea089a86-dbaf-454b-9d87-d4ce5cca4bb1_large.jpg?v=1460043054"></img>
+				</a>
+			</div>
+			<div className="col-xs-6 col-md-3">
+				<a href="#" className="thumbnail">
+				<img src="https://cdn.shopify.com/s/files/1/0249/1218/products/Vitamin-C-Serum-BoB_ea089a86-dbaf-454b-9d87-d4ce5cca4bb1_large.jpg?v=1460043054"></img>
+				</a>
+			</div>
+		</div>
+	</div>
+	
 );
