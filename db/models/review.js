09efa25374bcb8f37d6bc89193c3dc759be93356
anniejiwon <anniejiwon@gmail.com>
2016-11-01 16:33:27 -0400

    created reviews model

---
+++ db/models/review.js
@@ -0,0 +1,25 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+module.exports = db.define('reviews',{
+    subject: {
+        type: Sequelize.STRING,
+        defaultValue: 'No subject'
+    },
+    content: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    date: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    star: {
+        type: Sequelize.INTEGER,
+        defaultValue: null,
+        allowNull: false
+    }
+})
+
