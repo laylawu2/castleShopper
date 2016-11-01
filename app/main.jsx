c60c432609b1ce1e93313b5ed8a8c2b4936f98c1
jiayuliu27 <jiayuliu@umich.edu>
2016-11-01 17:46:55 -0400

    frontend routes

--- app/main.jsx
+++ app/main.jsx
@@ -4,6 +4,9 @@ import {render} from 'react-dom'
 import { Provider } from 'react-redux'
 import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
 import HomepageContainer from './containers/HomepageContainer';
+import UserContainer from './containers/UserContainer';
+import CartContainer from './containers/CartContainer';
+import CheckoutContainer from './containers/CheckoutContainer';
 
 import store from './store'
 
@@ -14,6 +17,9 @@ render (
 	      <Route path="/">
 	        <IndexRedirect to="/home" />
 	        <Route path="home" component={ HomepageContainer } />
+	        <Route path="/user" component={ UserContainer } />
+	        <Route path="/cart" component={ CartContainer } />
+	        <Route path="/checkout" component={ CheckoutContainer } />
 	      </Route>
 	    </Router>
 	  </Provider>
