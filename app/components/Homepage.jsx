b8eef57e6ce119f22bdbe79641dfe1c70baca778
anniejiwon <anniejiwon@gmail.com>
2016-11-02 18:22:02 -0400

    updated homepage no known bugs

--- app/components/Homepage.jsx
+++ app/components/Homepage.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
+import Navbar from './Navbar'
+import Star from './Star'
 export default ({allCastles}) => (
 	<div className="home">
-		<nav className="navbar navbar-default navbar-static-top" id="navigation">
-			<div className="container-fluid">
-				<div className="navbar-header">
-					<ul className="nav nav-pills">
-						<li role="presentation"><a href="#">Sign in</a></li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<Navbar />
 		<div id="header">CASTLESHOPPER</div>
 		<div className="row" id="homepage">
 			{
 				allCastles && allCastles.map(function(castle) {
-					return (<div key={castle.id} className="col-xs-6 col-md-3">
+					return (
+						<div key={castle.id} className="col-xs-6 col-md-3">
 							<a href="#" className="img-thumbnail" id="castle-photo">
-				<img src="http://akns-images.eonline.com/eol_images/Entire_Site/2016225/rs_300x300-160325124422-300-wizarding-world-of-harry-potter-hollywood.jm.32516.jpg" />
-				</a>
-				<p className="description" id="castle-title">{castle.name}</p>
-				<p>{castle.description}</p>
-					</div>)
+							<img src="http://akns-images.eonline.com/eol_images/Entire_Site/2016225/rs_300x300-160325124422-300-wizarding-world-of-harry-potter-hollywood.jm.32516.jpg" />
+							</a>
+							<p className="description" id="castle-title">{castle.name}</p>
+							<p>{castle.description}</p>
+							<Star />
+						</div>
+					)
 				})
 			}
 		</div>
---
+++ app/components/Star.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+export default ({}) => (
+	<div className="star-rating">
+        <div className="star-ratings">
+            <div className="star-ratings-top">
+            <span>★</span><span>★</span><span>★</span><span>★</span><span>★</span></div>
+            </div>
+	</div>
+);
