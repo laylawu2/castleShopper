42af741c1180ed60f1fc9790c205a75b8eae825b
jiayuliu27 <jiayuliu@umich.edu>
2016-11-03 13:14:14 -0400

    add review routes

---
+++ server/review.js
@@ -0,0 +1,47 @@
+const review = require('express').Router();
+const Review = require('../db/models/reivew');
+
+review.route('/')
+	.get((req, res, next) => {
+		Review
+			.findAll()
+			.then(reviews => reviews.json())
+			.then(reviews => res.status(200).send(reviews))
+			.catch(next);
+	});
+
+review.route('/:castleId')
+	.get((req, res, next) => {
+		Review
+			.findAll({
+				where: {
+					castleId: req.params.castleId
+				}
+			})
+			.then(reviews => reivews.json())
+			.then(reviews => res.status(200).send(reviews))
+			.catch(next);
+	})
+	.post((req, res, next) => {
+		req.body.castleId = req.params.castleId;
+		Review
+			.create(req.body)
+			.then(review => res.status(201).send(review))
+			.catch(next);
+	});
+
+review.route('/:userId')
+	.get((req, res, next) => {
+		Review
+			.findAll({
+				where: {
+					userId: req.params.userId
+				}
+			})
+			.then(reviews => reivews.json())
+			.then(reviews => res.status(200).send(reviews))
+			.catch(next);
+	});
+
+
+module.exports = review;
